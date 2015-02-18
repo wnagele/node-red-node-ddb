2b440ad28bff3a0bebca352c877c1c007b29a71b
Wolfgang Nagele <mail@wnagele.com>
2015-02-18 16:55:03 +1100

    Upgrade to use new credentials framework (spec: http://nodered.org/docs/creating-nodes/credentials.html)

--- 69-ddbout.js
+++ 69-ddbout.js
@@ -14,29 +14,34 @@
  * limitations under the License.
  **/
 
-var RED = require(process.env.NODE_RED_HOME+"/red/red");
-var util = require("util");
-var aws = require("aws-sdk");
-var attrWrapper = require("dynamodb-data-types").AttributeValue;
+module.exports = function(RED) {
+    var util = require("util");
+    var aws = require("aws-sdk");
+    var attrWrapper = require("dynamodb-data-types").AttributeValue;
 
-function DDBOutNode(n) {
-    RED.nodes.createNode(this, n);
-    this.credentials = RED.nodes.getNode(n.credentials);
-    this.region = n.region || "us-east-1";
-    this.table = n.table;
+    function DDBOutNode(n) {
+        RED.nodes.createNode(this, n);
+        this.region = n.region || "us-east-1";
+        this.table = n.table;
 
-    aws.config.update({ accessKeyId: this.credentials.accessKey,
-                        secretAccessKey: this.credentials.secretAccessKey,
-                        region: this.region });
+        aws.config.update({ accessKeyId: this.credentials.accessKey,
+                            secretAccessKey: this.credentials.secretAccessKey,
+                            region: this.region });
 
-    var ddb = new aws.DynamoDB();
+        var ddb = new aws.DynamoDB();
 
-    this.on("input", function(msg) {
-        ddb.putItem({ "TableName": this.table,
-                      "Item": attrWrapper.wrap(msg.payload) },
-            function(err, data) {
-                if (err) { util.log(err); }
+        this.on("input", function(msg) {
+            ddb.putItem({ "TableName": this.table,
+                          "Item": attrWrapper.wrap(msg.payload) },
+                function(err, data) {
+                    if (err) { util.log(err); }
+            });
         });
+    }
+    RED.nodes.registerType("ddb out", DDBOutNode, {
+        credentials: {
+            accessKey: { type:"text" },
+            secretAccessKey: { type: "password" }
+        }
     });
-}
-RED.nodes.registerType("ddb out", DDBOutNode);
+};
